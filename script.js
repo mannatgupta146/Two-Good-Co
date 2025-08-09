4ee40215c17b0d495dc0b83d9acac4b625e43ff3
mannatgupta146 <mannatgupta146@gmail.com>
2025-08-09 17:41:13 +0530

    added

--- script.js
+++ script.js
@@ -83,15 +83,24 @@ gsap.from("#page1 #video-container", {
 }
 page1loading();
 
-document.addEventListener("mousemove",function(dets){
-  gsap.to("#cursor",{
-    left:dets.x,
-    top:dets.y,
-  })
-})
-
-document.querySelector("#child1").addEventListener("mouseenter",function(){
-  gsap.to("#cursor",{
-    transform:"translate(-50%,-50%) scale(1)",
-  })
-})
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#cursor", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+  document.querySelectorAll(".child").forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(1)",
+      });
+    });
+    elem.addEventListener("mouseleave", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(0)",
+      });
+    });
+  });
+}
+cursorAnimation();
